db05e517fbc5f8df822cd470d0fb84231ad788fb
Maca-Acha <achamariamacarena@gmail.com>
2022-11-01 21:44:29 -0300

    Button-comenzar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Home from './pages/Home.jsx'
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Home />
+      </div>
+    </Router>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import logo from '../assets/logo-horizontal.png'
 import ilustracion from '../assets/ilustracion_intro.svg'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <div className='back-white'>
-            <div className='cont cont-home'>
-                <div className='space'></div>
-                <div className='info-home'>
-                    <h1 className='titles'>Te damos la bienvenida a</h1>
-                    <img alt="logo-horizontal" src={logo} className="logo-horizontal" />
-                    <img alt="ilustracion" src={ilustracion} className="ilustracion-intro" />
-                </div>
-                <div className='cont-button-home'>
-                    <button className='button-comenzar'>Comenzar el recorrido</button>
+            <div className='back-white'>
+                <div className='cont cont-home'>
+                    <div className='space'></div>
+                    <div className='info-home'>
+                        <h1 className='titles'>Te damos la bienvenida a</h1>
+                        <img alt="logo-horizontal" src={logo} className="logo-horizontal" />
+                        <img alt="ilustracion" src={ilustracion} className="ilustracion-intro" />
+                    </div>
+                    <div className='cont-button-home'>
+                        <div>
+                            <Link className='button-comenzar'>Comenzar el recorrido</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
